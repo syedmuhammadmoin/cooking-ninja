ee1080aacfc27ea652c69ca82afcd3ea61d464eb
Syed Moeen <syedmuhammadmoin@gmail.com>
2022-03-12 11:52:49 +0500

    new updation

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -21,7 +21,7 @@ export function ThemeProvider({ children }) {
   };
 
   return (
-    <ThemeContext.Provider value={{ color: 'blue' }}>
+    <ThemeContext.Provider value={{ ...state, changeColor }}>
       {children}
     </ThemeContext.Provider>
   );
