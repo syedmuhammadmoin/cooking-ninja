42e229b394217990a5885faba877cf7307ab63d8
Syed Moeen <syedmuhammadmoin@gmail.com>
2022-03-12 11:51:38 +0500

    use reducer updated

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,8 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
+  const themeReducer = (state, action) => {
+    switch (action.type) {
+      case 'CHANGE_COLOR':
+        return { ...state, color: action.payload };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: 'blue',
+  });
+
+  const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color });
+  };
+
   return (
     <ThemeContext.Provider value={{ color: 'blue' }}>
       {children}
